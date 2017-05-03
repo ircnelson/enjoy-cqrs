fe6680e05c03efd9e89a321930df0d922c93011f
Eric Swann <eric.swann@q2ebanking.com>
2017-05-03 00:21:51 -0500

    Attempting to build for correct framework

--- build_travis.sh
+++ build_travis.sh
@@ -38,6 +38,24 @@ if [ ! -d "$TOOLS_DIR" ]; then
   mkdir "$TOOLS_DIR"
 fi
 
+###########################################################################
+# INSTALL .NET CORE CLI
+###########################################################################
+
+#echo "Installing .NET CLI..."
+if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
+  mkdir "$SCRIPT_DIR/.dotnet"
+fi
+
+curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/v1.0.3/scripts/obtain/dotnet-install.sh
+
+sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version 1.0.3 --install-dir .dotnet --no-path
+export PATH="$SCRIPT_DIR/.dotnet":$PATH
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
+"$SCRIPT_DIR/.dotnet/dotnet"
+"$SCRIPT_DIR/.dotnet/dotnet" --info
 
 ###########################################################################
 # INSTALL NUGET
